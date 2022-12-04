86301be2626929f19f278a31d39198b3a057a0c2
Coby Konkol <ckonkol@ippathways.com>
2022-12-04 16:35:19 -0600

    got messages sending to angular UI

--- Python-Socket/SockioServer.py
+++ Python-Socket/SockioServer.py
@@ -2,27 +2,50 @@ import socket
 
 import eventlet
 import socketio
+import CyBotSock
+import threading
+import socket
+import re
+from flask import Flask
+import queue
+from gevent import pywsgi
+
+eventlet.monkey_patch()
 
 
+sio = socketio.Server(cors_allowed_origins='*')
 
 
+startChar = "start"
+endChar = "end"
+
+def bg_emit(_str):
+    sio.emit('message', _str)
+def listen(sock):
+    while True:
+        line = re.split("start.+?end", sock.recv(1024).decode('utf8'))
+        for l in line:
+            print("found: ", str(l))
+            bg_emit(l)
+            eventlet.sleep(2)
 
 
 class CyBotSocketServer():
     def __init__(self):
-        self.sio = socketio.Server()
-        self.app = socketio.WSGIApp(self.sio)
 
-        @self.sio.event
+        self.app = socketio.WSGIApp(sio)
+
+        @sio.event
         def connect(sid, environ):
-            self.sio.emit("message", "hi")
+            sio.emit("message", "Welcome")
             print('connect ', sid)
 
-        @self.sio.event
+        @sio.event
         def my_message(sid, data):
-            print('message ', data)
+            s.emit(bytes(data, "utf-8"))
+            print("message", data)
 
-        @self.sio.event
+        @sio.event
         def disconnect(sid):
             print('disconnect ', sid)
 
@@ -30,10 +53,12 @@ class CyBotSocketServer():
 
 
 if __name__ == "__main__":
-    useCybot = False
+    useCybot = True
     if useCybot:
+        global s
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         s.connect(("192.168.1.1", 288))
         print("Connected")
+        eventlet.spawn(listen, s)
 
     server = CyBotSocketServer()
